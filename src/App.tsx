44f7dc542e8a7cb346f8631a3e86095cb61b6fb5
taihk2@gmail.com <taihk2@gmail.com>
2025-07-18 14:24:47 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
